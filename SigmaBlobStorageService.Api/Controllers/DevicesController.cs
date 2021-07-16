b8a870078c588ee216dd3876faea8abb8bce8a0a
Johnny205 <grabmar97@gmail.com>
2021-07-16 14:46:43 +0200

    Dodaj pliki projektów.

---
+++ SigmaBlobStorageService.Api/Controllers/DevicesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SigmaBlobStorageService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DevicesController : ControllerBase
+    {
+
+        private readonly ILogger<DevicesController> _logger;
+
+        public DevicesController(ILogger<DevicesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "get value";
+        }
+    }
+}
